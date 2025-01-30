1539a46a1d954882b41c87a728b50f8db3d570b8
Christina Koch <christinakconnect@gmail.com>
2025-01-30 12:49:46 -0600

    download container with pelican

--- software/download_software.sh
+++ software/download_software.sh
@@ -1,7 +1,5 @@
 # Script name: download_software.sh
 
-SIF_URL=osdf:///ospool/uc-shared/OSG-staff/something/fastqc-v2.sif
+SIF_URL=osdf:///ospool/uc-shared/public/osg-training/tutorial-fastqc/sif/fastqc-v1.sif
 
-cd software
 pelican object get $SIF_URL software/fastqc.sif
-cd ..
